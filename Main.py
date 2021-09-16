7ad41fbae9f69fbb2f03a579f3b54b000cacbb5a
Marius Luft <luftmarius97@gmail.com>
2021-09-16 18:23:06 +0200

    adds animation for the grid to move towards the player

--- Main.py
+++ Main.py
@@ -3,19 +3,23 @@ from kivy.uix.widget import Widget
 from kivy.properties import NumericProperty
 from kivy.graphics.vertex_instructions import Line
 from kivy.graphics.context_instructions import Color
+from kivy.properties import Clock
 
 class MainWidget(Widget):
     perspecitvePointX = NumericProperty(0)
     perspecitvePointY = NumericProperty(0)
 
     verticalLines = []
-    numVerticalLines = 4
-    spacingVerticalLines = 0.1
+    numVerticalLines = 10
+    spacingVerticalLines = 0.25
 
     
     horizontalLines = []
-    numhorizontalLines = 4
-    spacinghorizontalLines = 0.2
+    numhorizontalLines = 15
+    spacinghorizontalLines = 0.1
+
+    currentOffsetY = 0
+    speed = 3
 
 
     def __init__(self, **args):
@@ -23,6 +27,7 @@ class MainWidget(Widget):
         #print("init width: " + str(self.width) + " init height: " + str(self.height))
         self.initVerticalLines()
         self.initHorizontalLines()
+        Clock.schedule_interval(self.update, 1.0/60.0)
 
     def on_parent(self, widget, parent):
         pass
@@ -31,8 +36,8 @@ class MainWidget(Widget):
         #print("init width: " + str(self.width) + " init height: " + str(self.height))
         self.perspecitvePointX = self.width/2
         self.perspecitvePointY = self.height*0.75
-        self.updateVerticalLines()
-        self.updateHorizontalLines()
+
+        
 
     def on_perspecitvePointX(self, widget, value):
         #print("PX: " +  str(value))
@@ -56,7 +61,7 @@ class MainWidget(Widget):
         #calculate the spacing
         spacing = self.spacingVerticalLines * self.width
         # offset to start from the left line
-        offset = -int(self.numVerticalLines/2) + 0.5 
+        offset = -int(self.numVerticalLines/2) + 0.5 # + 0.5 to shift half a spacing
 
         for i in range(0, self.numVerticalLines):
             lineX = int(centerX + offset* spacing)
@@ -74,40 +79,58 @@ class MainWidget(Widget):
                 self.horizontalLines.append(Line())
 
     def updateHorizontalLines(self):
-        xMin = 0
-        xMax = self.width
+        #find middle
+        centerX = self.width/2
+        #calculate the spacing
+        spacing = self.spacingVerticalLines * self.width
+        # offset to start from the left line
+        offset = -int(self.numVerticalLines/2) + 0.5 # + 0.5 to shift half a spacing
+
+        spacingY = self.spacinghorizontalLines * self.height
+        xMin = centerX + spacing*offset
+        xMax = centerX - spacing*offset
 
         for i in range(0, self.numhorizontalLines):
-            lineY = int(i * self.spacinghorizontalLines * self.height)
+            lineY = int(i * spacingY - self.currentOffsetY)
             x1, y1 = self.transform(xMin, lineY)
             x2, y2 = self.transform(xMax, lineY)
             self.horizontalLines[i].points = [x1,y1,x2, y2]
 
     def transform(self, x, y):
-        # return self.transformPerspective(x, y)
-        return self.transform2D(x,y)
+        return self.transformPerspective(x, y)
+        # return self.transform2D(x,y)
 
     def transform2D(self, x, y):
         return int(x), int(y)
     
     def transformPerspective(self, x, y):
         # sets 3d y coordinate
-        transformedY = y * self.perspecitvePointY / self.height
+        linearY = y * self.perspecitvePointY / self.height
         # limits y to the perspective point
-        if transformedY > self.perspecitvePointY:
-            transformedY = self.perspecitvePointY
+        if linearY > self.perspecitvePointY:
+            linearY = self.perspecitvePointY
 
         # calculates distance to the perspective point
         diffX = x - self.perspecitvePointX
-        diffY = self.perspecitvePointY - transformedY
+        diffY = self.perspecitvePointY - linearY
 
         # gets proportional distance
-        proportionY = diffY/self.perspecitvePointY
+        factorY = diffY/self.perspecitvePointY
+        factorY = pow(factorY, 4)
 
         # calculates x based on proportional distance of y
-        transformedX = self.perspecitvePointX + diffX * proportionY
+        transformedX = self.perspecitvePointX + diffX * factorY
+        transformedY = (1- factorY) *self.perspecitvePointY
 
         return int(transformedX), int(transformedY)
+
+    def update(self, dt):
+        self.updateVerticalLines()
+        self.updateHorizontalLines()
+        self.currentOffsetY += self.speed
+        spacingY = self.spacinghorizontalLines * self.height
+        if self.currentOffsetY >= spacingY:
+            self.currentOffsetY -= spacingY
             
 
 class GalaxyApp(App):
@@ -116,5 +139,3 @@ class GalaxyApp(App):
 
 if __name__ == '__main__':    
     GalaxyApp().run()
-
-
